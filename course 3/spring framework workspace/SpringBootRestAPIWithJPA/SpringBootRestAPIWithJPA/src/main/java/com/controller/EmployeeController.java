c90a6591f4e1595ecc3568bf018c30b3093767ba
akash <akash300383@gmail.com>
2024-06-13 08:25:17 +0530

    done

--- course 3/spring framework workspace/SpringBootRestAPIWithJPA/SpringBootRestAPIWithJPA/src/main/java/com/controller/EmployeeController.java
+++ course 3/spring framework workspace/SpringBootRestAPIWithJPA/SpringBootRestAPIWithJPA/src/main/java/com/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -31,7 +32,7 @@ public class EmployeeController {
 	}
 	
 	@RequestMapping(value = "addEmployeeInDb",method = RequestMethod.POST)
-	public String addEmployeeInDb(HttpServletRequest req,Employee emp) {   // DI for request object as well as Employee  
+	public String addEmployeeInDb(HttpServletRequest req,Employee emp,Model model) {   // DI for request object as well as Employee  
 		
 		int id = Integer.parseInt(req.getParameter("id"));
 		String name = req.getParameter("name");
@@ -42,6 +43,7 @@ public class EmployeeController {
 		emp.setName(name);
 		
 		String result = employeeService.storeEmployee(emp);
+		model.addAttribute("msg", result);  // store result in model scope
 		System.out.println(result);
 		
 		return "addEmployee";		// viewresolver :return page with without extension
