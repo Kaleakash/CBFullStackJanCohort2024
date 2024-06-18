77f6e3da3c8be3da6b6c17a64b9817745e4599e6
akash <akash300383@gmail.com>
2024-06-18 07:26:40 +0530

    done

---
+++ course 3/spring framework workspace/SpringBootRestAPIWithData/SpringBootRestAPIWithData/src/main/java/com/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.Product;
+import com.service.ProductService;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	ProductService productService;
+	
+	@RequestMapping(value = "findAllProducts",produces = MediaType.APPLICATION_JSON_VALUE,method = RequestMethod.GET)
+	public List<Product> findAllProducts() {
+		return productService.findAllProducts();
+	}
+	@RequestMapping(value = "storeProduct",consumes = MediaType.APPLICATION_JSON_VALUE,method = RequestMethod.POST)
+	public String storeProduct(@RequestBody Product product) {
+		return productService.storeProduct(product);
+	}
+	
+}
+
+
+
