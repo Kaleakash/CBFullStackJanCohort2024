6d4d8eb55e8b8d290aac64f4c7c5baa3d79b0dd0
akash <akash300383@gmail.com>
2024-06-18 07:03:33 +0530

    done

---
+++ course 3/spring framework workspace/SpringBootRestAPIWithData/SpringBootRestAPIWithData/src/main/java/com/SpringBootRestApiWithDataApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiWithDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiWithDataApplication.class, args);
+	}
+
+}
