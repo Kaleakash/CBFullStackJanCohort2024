86dd411ff1dfcbe7ee306b3f03c80402ea32c87a
akash <akash300383@gmail.com>
2024-06-20 09:07:48 +0530

    done

--- course 3/spring framework workspace/SpringBootRestAPIWithData/SpringBootRestAPIWithData/src/main/java/com/controller/OrdersController.java
+++ course 3/spring framework workspace/SpringBootRestAPIWithData/SpringBootRestAPIWithData/src/main/java/com/controller/OrdersController.java
@@ -1,7 +1,11 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,20 @@ public class OrdersController {
 	@PostMapping(value = "place",consumes = MediaType.APPLICATION_JSON_VALUE)
 	public String placerOrder(@RequestBody Orders order) {
 		return ordersService.placeOrder(order);
+	}
+	
+	@GetMapping(value = "viewall",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Orders> viewAllOrders() {
+		return ordersService.viewAllOrders();
+	}
+	
+	@GetMapping(value = "viewbyorderid/{oid}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Orders viewOrderInfo(@PathVariable("oid") int oid) {
+		return ordersService.searchOrdersByOrderId(oid);
+	}
+	@GetMapping(value = "viewbyproductid/{pid}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Orders> viewOrderByProductId(@PathVariable("pid") int pid){
+		return ordersService.viewOrderByProductId(pid);
 	}
 	
 }
