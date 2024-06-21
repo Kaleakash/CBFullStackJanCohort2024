ce540c31b94dd049a92ccb9f3b4882bd928bcf0a
akash <akash300383@gmail.com>
2024-06-21 09:12:25 +0530

    done

--- course 3/spring micro service workspace/account-micro-service/account-micro-service/src/main/java/com/service/AccountService.java
+++ course 3/spring micro service workspace/account-micro-service/account-micro-service/src/main/java/com/service/AccountService.java
@@ -1,8 +1,11 @@
 package com.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.entity.Account;
 import com.repository.AccountRepository;
 
 @Service
@@ -10,4 +13,28 @@ public class AccountService {
 
 	@Autowired
 	AccountRepository accountRepository;
+	
+	public String createAccount(Account account) {
+		Optional<Account> result = accountRepository.findById(account.getAccno());
+		if(result.isPresent()) {
+			return "Account already exists";
+		}else {
+			accountRepository.save(account);
+			return "Account created successfully";
+		}
+	}
+	
+	public Account findAccount(int accno) {
+		Optional<Account> result = accountRepository.findById(accno);
+		if(result.isPresent()) {
+			Account acc = result.get();
+			return acc;
+		}else {
+			return null;
+		}
+	}
 }
+
+
+
+
