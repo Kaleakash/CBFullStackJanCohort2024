8846dfd75779ea6e4416ec2390072b07aab0a600
akash <akash300383@gmail.com>
2024-06-21 09:17:54 +0530

    done

--- course 3/spring micro service workspace/account-micro-service/account-micro-service/src/main/java/com/controller/AccountController.java
+++ course 3/spring micro service workspace/account-micro-service/account-micro-service/src/main/java/com/controller/AccountController.java
@@ -1,9 +1,15 @@
 package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.entity.Account;
 import com.service.AccountService;
 
 @RestController
@@ -12,4 +18,20 @@ public class AccountController {
 
 	@Autowired
 	AccountService accountService;
+	
+	@PostMapping(value = "create",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String createAccount(@RequestBody Account account) {
+		return accountService.createAccount(account);
+	}
+	
+	@GetMapping(value = "find/{accno}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Account findAccountDetails(@PathVariable("accno") int accno) {
+		return accountService.findAccount(accno);
+	}
 }
+
+
+
+
+
+
