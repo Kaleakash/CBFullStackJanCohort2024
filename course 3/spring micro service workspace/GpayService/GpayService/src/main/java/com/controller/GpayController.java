dae16aabc99c66c0e7e07e4475ab4325e2149109
akash <akash300383@gmail.com>
2024-06-25 08:25:55 +0530

    done

---
+++ course 3/spring micro service workspace/GpayService/GpayService/src/main/java/com/controller/GpayController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.Gpay;
+import com.service.GpayService;
+
+@RestController
+@RequestMapping("gpay")
+public class GpayController {		//http://localhost:8484/gpay/*
+
+	@Autowired
+	GpayService gpayService;
+	
+	@PostMapping(value = "create",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String createGpayAccount(@RequestBody Gpay gpay) {
+		return gpayService.createGpayAccount(gpay);
+	}
+}
--- course 3/spring micro service workspace/GpayService/GpayService/src/main/java/com/service/GpayService.java
+++ course 3/spring micro service workspace/GpayService/GpayService/src/main/java/com/service/GpayService.java
@@ -18,7 +18,9 @@ public class GpayService {
 	
 	public String createGpayAccount(Gpay gpay) {		// only emailid present
 		String emailid = gpay.getEmailid();
+		//restTemplate.postForEntity("", gpay, String.class);
 		int accno = restTemplate.getForObject("http://localhost:8383/account/findbyemailid/"+emailid, Integer.class);
+		
 		if(accno==-1) {
 			gpayRepository.save(gpay);
 			return "gpay account created without account register";  // emailid, but accno=0
