ed721e99d64a576424ce160c8674144c56d2f96b
akash <akash300383@gmail.com>
2024-06-25 08:35:30 +0530

    done

--- course 3/spring micro service workspace/GpayService/GpayService/src/main/java/com/GpayServiceApplication.java
+++ course 3/spring micro service workspace/GpayService/GpayService/src/main/java/com/GpayServiceApplication.java
@@ -2,12 +2,27 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.entity")
+@EnableJpaRepositories(basePackages = "com.repository")
+@EnableDiscoveryClient
 public class GpayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpayServiceApplication.class, args);
+		System.err.println("Gpay micro service up!");
 	}
 
+	@Bean					// this method is responsible to create the object of type RestTemplate 
+	public RestTemplate restTemplate() {
+		System.out.println("object created...");
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
 }
