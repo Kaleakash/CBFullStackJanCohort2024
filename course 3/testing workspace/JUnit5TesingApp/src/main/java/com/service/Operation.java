848dbc9f76f67a296af8ab876908ba697d8714ba
akash <akash300383@gmail.com>
2024-06-27 08:47:38 +0530

    done

---
+++ course 3/testing workspace/JUnit5TesingApp/src/main/java/com/service/Operation.java
@@ -0,0 +1,13 @@
+package com.service;
+
+public class Operation {
+
+	public int add(int a, int b) {
+		int sum = a+b;
+		return sum;
+	}
+	public int sub(int a, int b) {
+		int sub = a-b;
+		return sub;
+	}
+}
