fd755d6e7b8657861a9288961e09f8dd7173fcb9
akash <akash300383@gmail.com>
2024-06-28 07:22:37 +0530

    done

--- course 3/testing workspace/login-app-spring-boot-testing/login-app-spring-boot-testing/src/main/java/com/entity/Login.java
+++ course 3/testing workspace/login-app-spring-boot-testing/login-app-spring-boot-testing/src/main/java/com/entity/Login.java
@@ -1,7 +1,24 @@
 package com.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Login {
+@Id
 private String emailid;
 private String password;
+public String getEmailid() {
+	return emailid;
+}
+public void setEmailid(String emailid) {
+	this.emailid = emailid;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
 
 }
