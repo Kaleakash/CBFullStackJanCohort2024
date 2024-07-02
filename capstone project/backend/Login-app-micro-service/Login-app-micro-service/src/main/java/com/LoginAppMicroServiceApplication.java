876c3d886e9fdf241910154a8a42a2b6c232691a
akash <akash300383@gmail.com>
2024-07-02 07:20:55 +0530

    done

---
+++ capstone project/backend/Login-app-micro-service/Login-app-micro-service/src/main/java/com/LoginAppMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAppMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAppMicroServiceApplication.class, args);
+	}
+
+}
