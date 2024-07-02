ee956ee2bed652c8ab5f1f66960e768d2a70c167
akash <akash300383@gmail.com>
2024-07-02 08:27:36 +0530

    done

--- capstone project/backend/Login-app-micro-service/Login-app-micro-service/src/main/java/com/LoginAppMicroServiceApplication.java
+++ capstone project/backend/Login-app-micro-service/Login-app-micro-service/src/main/java/com/LoginAppMicroServiceApplication.java
@@ -2,12 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.eshopping")
+@EntityScan(basePackages = "com.eshopping.entity")
+@EnableJpaRepositories(basePackages = "com.eshopping.repository")
+@EnableDiscoveryClient(autoRegister = true)
 public class LoginAppMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginAppMicroServiceApplication.class, args);
+		System.err.println("login micro service up on port number 8181");
 	}
 
 }
