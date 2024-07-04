8cb2712dd5430e23eb1c181a857f9a37ae37bf79
akash <akash300383@gmail.com>
2024-07-04 07:36:28 +0530

    done

--- capstone project/frontend/shopping-frontend-app/src/app/login/login.component.ts
+++ capstone project/frontend/shopping-frontend-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+loginRef = new FormGroup({
+emailid:new FormControl(),
+password:new FormControl(),
+typeofuser:new FormControl()
+});
+
+signin(): void {
+
+  let login = this.loginRef.value;
+  console.log(login);   // in the form of json 
+
+  // coding 
+
+  this.loginRef.reset();
+}
 
 }
