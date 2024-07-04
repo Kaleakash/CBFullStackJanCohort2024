870fa86193ca709491d5857cfae384a333d27e5b
akash <akash300383@gmail.com>
2024-07-04 09:05:52 +0530

    done

--- capstone project/frontend/shopping-frontend-app/src/app/signup/signup.component.ts
+++ capstone project/frontend/shopping-frontend-app/src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,10 +9,24 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent {
 
+  msg:string ="";
+  constructor(public ls:LoginService){} // DI for login service 
 
   signup(loginref:NgForm): void {
     let login = loginref.value;
     console.log(login);
+
+    this.ls.signUp(login).subscribe({
+      next:(result:any)=> {
+        this.msg=result
+      },
+      error:(error:any)=> {
+        console.log(error)
+      },
+      complete:()=> {
+        console.log("signup done!")
+      }
+    })
 
     loginref.reset();
   }
